76fa7db9ec63dc754ea53f9d15a461e5536d41c7
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-12 13:40:18 -0800

    return city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
+var myApi = "1ba7fe7548e5920eb3df1299e7ccc9f0";
+
 function getSearch() {
   event.preventDefault();
   var searchVal = document.querySelector("#search").value;
   saveCity(searchVal);
+  searchWeather(searchVal);
 };
 
 function saveCity(searchVal) {
@@ -14,5 +17,14 @@ function saveCity(searchVal) {
   var history = document.querySelector("#city-history");
   history.appendChild(li);
 };
+
+function searchWeather(searchVal) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchVal}&appid=${myApi}&units=imperial`)
+    .then((response) => {
+      return response.json();
+    }).then((cityData) => {
+      console.log('City Data', cityData);
+    })
+}
 
 document.querySelector("#searchBtn").addEventListener("click", getSearch);
