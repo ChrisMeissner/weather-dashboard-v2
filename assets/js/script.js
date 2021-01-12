cf6b62b87f75dbd0288ed5a43dbfbadad16c5514
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-12 13:33:26 -0800

    add save city functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 function getSearch() {
   event.preventDefault();
   var searchVal = document.querySelector("#search").value;
-  console.log(searchVal);
-} ;
+  saveCity(searchVal);
+};
+
+function saveCity(searchVal) {
+  var li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.textContent = searchVal;
+  li.onclick = function() {
+      saveCity(searchVal);
+  };
+  var history = document.querySelector("#city-history");
+  history.appendChild(li);
+};
 
 document.querySelector("#searchBtn").addEventListener("click", getSearch);
