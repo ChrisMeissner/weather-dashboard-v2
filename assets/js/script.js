d75e6f18dd493a1c51e91718f58713793ba1e8bd
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-12 19:11:52 -0800

    last https redo

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,7 +116,7 @@ var getUV = function(lat, lon) {
 };
 
 var fiveDayForecast = function(searchVal){
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchVal}&appid=${myApi}`)
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchVal}&appid=${myApi}`)
     .then((response) => {
         console.log(response);
         return response.json();
